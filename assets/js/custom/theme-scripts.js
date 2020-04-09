f2e172ae4a735625dbf11efb606ba42815835549
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-04-08 20:30:23 -0500

    updating

--- assets/js/custom/theme-scripts.js
+++ assets/js/custom/theme-scripts.js
@@ -9,7 +9,7 @@
 --------------------------------------------------------------*/
 import $ from 'jquery';
 import "slick-carousel/slick/slick.js";
-import "isotope-layout";
+import isotope from "isotope-layout";
 
 	console.log('Made with love by bloom!!');
 
