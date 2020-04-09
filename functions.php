62d2d2a0cd13231dd0f4bc7a5d697bfe5046abe7
Oswaldo Zarate <oswaldozarate@Oswaldos-MacBook-Air.local>
2020-04-08 20:02:55 -0500

    updating

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function grupomb_scripts() {
 	wp_enqueue_script( 'fontawesome-js', 'https://pro.fontawesome.com/releases/v5.1.0/js/fontawesome.js', '', '', true );
 	// wp_enqueue_script( 'vendors-js', get_template_directory_uri() . '/js/vendor/vendors.min.js', array('jquery'), '', true );
 	wp_enqueue_script( 'isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', '', '', true);
-	wp_enqueue_script( 'grupomb_js', get_template_directory_uri() . '/js/theme-scripts.js', '', '', true );
+	wp_enqueue_script( 'grupomb_js', get_template_directory_uri() . '/js/theme-scripts.js', array('isotope'), '', true );
 
 	if(is_page('contacto')) {
 		wp_enqueue_script( 'google_maps', 'https://maps.googleapis.com/maps/api/js?key=' . get_field('google_maps_api_key', 'option') . '&callback=initMap', '', '', true );
